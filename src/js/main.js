74d1ba606038e81710f36826f6217977bba60712
Timur Sharipov <sharipow03@mail.ru>
2026-06-02 00:58:20 +0500

    init starter

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+//! Js libs
+// jQuery
+import $ from 'jquery'
+// Swiper
+import Swiper from 'swiper'
+import 'swiper/css'
+
+//! Css
+// Main scss
+import '../scss/main.scss'
+
+$(document).ready(() => {
+    console.log('Ready')
+})
