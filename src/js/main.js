d2b8c827f05b424dc257a3ab92c06a360910f1c2
Timur Sharipov <sharipow03@mail.ru>
2026-06-30 05:24:09 +0500

    added aos and jquery-mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
 //! Js libs
 // jQuery
 import $ from 'jquery'
+// jQuery Mask
+import 'jquery-mask-plugin'
+
 // Swiper
 import Swiper from 'swiper'
+import { Navigation, Pagination, Autoplay } from 'swiper/modules'
 import 'swiper/css'
+
+// AOS
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 //! Css
 // Main scss
